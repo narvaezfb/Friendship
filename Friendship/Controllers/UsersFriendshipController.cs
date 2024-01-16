42737c3cd653805bcf7924349126eb0a99f455e3
Fabian Narvaez <narvaezfb4@hotmail.com>
2024-01-16 15:53:59 -0500

    Friendships endpoints added

--- Friendship/Controllers/UsersFriendshipController.cs
+++ Friendship/Controllers/UsersFriendshipController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Friendship.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Friendship.Controllers
 {
@@ -14,6 +15,55 @@ namespace Friendship.Controllers
 			_context = context;
 		}
 
+		[HttpGet("Friendships/{UserId}")]
+		public async Task<ActionResult> GetUserFriendships(string UserId)
+		{
+			try
+			{
+                var friendships = await _context.UsersFriendships.Where(uf => (uf.UserId1 == UserId || uf.UserId2 == UserId))
+            											         .OrderBy(uf => uf.DateCreated)
+            												     .ToListAsync();
+				if(!friendships.Any() || friendships == null)
+				{
+					return NotFound("User Does not have any frienships");
+				}
+				return Ok(friendships);
+            }
+            catch (DbUpdateException ex)
+			{
+				return StatusCode(500, $"Database error: {ex.Message}");
+			}
+			catch(Exception ex)
+			{
+				return StatusCode(500, $"Internal Server Error: {ex.Message}");
+			}
+		}
+		[HttpDelete("Friendship/{FriendshipId}")]
+		public async Task<ActionResult> DeleteFriendship(string FriendshipId)
+		{
+			try
+			{
+				var friendship = await _context.UsersFriendships.FindAsync(FriendshipId);
+
+				if(friendship == null)
+				{
+					return NotFound("Not friendship found with that ID");
+				}
+
+				_context.UsersFriendships.Remove(friendship);
+				await _context.SaveChangesAsync();
+
+				return Ok("Friendship Deleted successfully");
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, $"Database error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal Server Error: {ex.Message}");
+            }
+        }
 	}
 }
 
